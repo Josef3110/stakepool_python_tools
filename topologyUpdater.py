3d5d53f47cbc942467fc8f4af009922a7679b68c
Josef Fritscher <admin@adapool.at>
2021-04-19 17:15:19 +0200

    improved error handling

--- topologyUpdater.py
+++ topologyUpdater.py
@@ -2,6 +2,7 @@
 #
 # topologyUpdater as python variant
 
+import os
 import sys
 import argparse
 import requests
@@ -11,14 +12,24 @@ from datetime import datetime
 
 # define some constants upfront
 
-cnode_valency = "1"
-max_peers = "15"
-nwmagic = "764824073"       # network magic for mainnet - if you want a testnet then change the value in config file instead
+cnode_valency_default = "1"
+max_peers_default = "15"
+nwmagic_default = "764824073"       # network magic for mainnet - if you want a testnet then change the value in config file instead
+
+def timestamp4log():
+    return datetime.now().strftime("%d.%m.%Y %H:%M:%S");
+
+def exception_handler(exception_type, exception, traceback):
+    # All your trace are belong to us!
+    # your format
+    print(os.path.basename(sys.argv[0]) + "@" + timestamp4log() + ": " + exception_type.__name__, exception)
+
+sys.excepthook = exception_handler
 
 def getconfig(configfile):
     with open(configfile) as f:
         data = json.load(f)
-        f.close()
+    f.close()
     return data;
 
 def requestmetric(url):
@@ -26,16 +37,16 @@ def requestmetric(url):
         response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.HTTPError as errh:
-        print(datetime.now().strftime("%d.%m.%Y %H:%M:%S") + " Http Error: " + repr(errh))
+        print(timestamp4log() + ": Http Error: " + repr(errh))
         sys.exit()
     except requests.exceptions.ConnectionError as errc:
-        print(datetime.now().strftime("%d.%m.%Y %H:%M:%S") + " Error Connecting: " + repr(errc))
+        print(timestamp4log() + ": Error Connecting: " + repr(errc))
         sys.exit()
     except requests.exceptions.Timeout as errt:
-        print(datetime.now().strftime("%d.%m.%Y %H:%M:%S") + " Timeout Error: " + repr(errt))
+        print(timestamp4log() + ": Timeout Error: " + repr(errt))
         sys.exit()
     except requests.exceptions.RequestException as err:
-        print(datetime.now().strftime("%d.%m.%Y %H:%M:%S") + " OOps: Something Else " + repr(err))
+        print(timestamp4log() + ": OOps: Something Else " + repr(err))
         sys.exit()
     finally:
         return response;
@@ -49,22 +60,53 @@ parser.add_argument("-v", "--version", help="displays version and exits", action
 parser.add_argument("-c", "--config", help="path to config file in json format", default="topologyUpdater.json")
 
 args = parser.parse_args()
+myname = os.path.basename(sys.argv[0])
 
 if args.version:
-    print("topologyUpdater.py version 0.3")
+    print(myname + " version 0.4")
     sys.exit()
+
+if not args.push and not args.fetch:
+    do_both = True
+else:
+    do_both = False
 
 if args.config:
     my_config = getconfig(args.config)
-    cnode_hostname = my_config['hostname']
-    cnode_port = my_config['port']
-    cnode_valency = my_config['valency']
-    max_peers = my_config['maxPeers']
-    destination_topology_file = my_config['destinationTopologyFile']
-    logfile = my_config['logfile']
-    nwmagic = my_config['networkMagic']
+    if 'hostname' in my_config:
+        cnode_hostname = my_config['hostname']
+    else:
+        print(myname + "@" + timestamp4log() + ": relay hostname parameter is missing in configuration")
+        sys.exit()
+    if 'port' in my_config:
+        cnode_port = my_config['port']
+    else:
+        print(myname + "@" + timestamp4log() + ": relay port parameter is missing in configuration")
+        sys.exit()
+    if 'valency' in my_config:
+        cnode_valency = my_config['valency']
+    else:
+        cnode_valency = cnode_valency_default
+    if 'maxPeers' in my_config:
+        max_peers = my_config['maxPeers']
+    else:
+        max_peers = max_peers_default
+    if 'destinationTopologyFile' in my_config:
+        destination_topology_file = my_config['destinationTopologyFile']
+    else:
+        print(myname + "@" + timestamp4log() + ": destination for topology file is missing in configuration")
+        sys.exit()
+    if 'logfile' in my_config:
+        logfile = my_config['logfile']
+    else:
+        print(myname + "@" + timestamp4log() + ": path for logfile is missing in configuration")
+        sys.exit()
+    if 'networkMagic' in my_config:
+        nwmagic = my_config['networkMagic']
+    else:
+        nwmagic = nwmagic_default
 
-if args.push:
+if args.push or do_both:
     # first get last block number
     response = requestmetric('http://localhost:12798/metrics')
 
@@ -93,12 +135,12 @@ if args.push:
     #if (parsed_response['resultcode'] != '204'):
     #    print("got resultcode = " + parsed_response['resultcode'])
 
-if args.fetch:
+if args.fetch or do_both:
     url = "https://api.clio.one/htopology/v1/fetch/?max=" + max_peers + "&magic=" + nwmagic
     response = requests.get(url)
     parsed_response = json.loads(response.text)
     if (parsed_response['resultcode'] != '201'):
-        print("got wrong resultcode = " + parsed_response['resultcode'])
+        print(myname + "@" + timestamp4log() + ": got wrong resultcode = " + parsed_response['resultcode'])
         sys.exit()
 
     # now we have to add custom peers
