75713f4b39d75b4ee1cb67881433bba06e6127ba
Josef Fritscher <admin@adapool.at>
2022-02-16 10:31:20 +0100

    print token balance as well now.

--- balance.py
+++ balance.py
@@ -8,8 +8,9 @@ import re
 import subprocess
 import argparse
 import json
+import base64
 
-version = "0.3"
+version = "0.6"
 ada2lovelace = 1000000
 
 def do_query(what,parameter,magic,debug):
@@ -65,16 +66,35 @@ if float(tip['syncProgress']) < 100.0:
 
 all_utxo = do_query("utxo",address,magic,print_debug).splitlines()
 skip_header = 0
-amount = 0
+amount_ada = 0
+token_value = 0
+token = {}
 
 for utxo in all_utxo:
     if (skip_header < 2):
         skip_header = skip_header + 1
     else:
         col = re.split(r'\s+',utxo)
-        amount = amount + int(col[2])
+        amount_ada = amount_ada + int(col[2])
+        number_of_token = int((len(col) - 5)/3)
+        i = 5
+        while i < (len(col)-2):
+                token_utxo = col[i+1].split('.')
+                token_name = token_utxo[1]
+                token_dec = str(base64.b16decode(token_name.encode('utf8').upper()).decode('utf8'))
+                token_amount = int(col[i])
+                if token_dec in token:
+                        token[token_dec] = token[token_dec] + token_amount
+                else:
+                        token[token_dec] = token_amount
+                i = i + 3
 
-print("balance is " + str(float(amount)/ada2lovelace) + " ADA")
+print("balance is " + str(float(amount_ada)/ada2lovelace) + " ADA")
+if token:
+        print("balance of token (without digits):")
+        for token_item in token.keys():
+                print("\t" + str(token[token_item]) + " " + token_item)
+
 if print_debug:
     if args.testnet_magic:
         print("on testnet with magic = " + str(args.testnet_magic))
