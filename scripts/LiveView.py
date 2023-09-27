004b723ad53487859202bc729eb03d260c21fe1a
Josef Fritscher <admin@adapool.at>
2023-09-28 01:16:21 +0200

    some more lines added

--- scripts/LiveView.py
+++ scripts/LiveView.py
@@ -12,17 +12,20 @@ import json
 import time
 from datetime import datetime
 
-VERSION = "0.1"
+VERSION = "0.2"
 NODE_NAME = "Cardano Node" 
 REFRESH_RATE = 2
 
 # define some default values upfront
 
 MAINNET_MAGIC = "764824073"                     # network magic for mainnet - if you want a testnet then change the value in config file instead
-METRICS_URL = "http://localhost:12788/metrics"
+METRICS_URL = "http://localhost:12788"
+HEADERS = {'Accept': 'application/json'}
 WIDTH = 71
 #char_marked = $(printf "\\u258C")
 #char_unmarked = $(printf "\\u2596")
+
+
 
 def get_node_version(debug):
     command = ["cardano-node", "version"]        
@@ -39,9 +42,14 @@ def get_node_version(debug):
 def timestamp4log():
     return datetime.now().strftime("%d.%m.%Y %H:%M:%S");
 
+def get_genesis(shelleyGenesis):
+    with open(shelleyGenesis) as f:
+        data = json.load(f)
+    return data;
+
 def requestmetric(url):
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers = HEADERS)
         response.raise_for_status()
     except requests.exceptions.HTTPError as errh:
         sys.exit()
@@ -52,7 +60,8 @@ def requestmetric(url):
     except requests.exceptions.RequestException as err:
         sys.exit()
     finally:
-        return response;
+        metrics = response.json()
+        return metrics;
 
 # main
 
@@ -67,6 +76,7 @@ parser.add_argument("-c", "--config", help="path to config file in json format",
 parser.add_argument("-t", "--testnet-magic", type=int, nargs='?', const=1, help="run on preprod testnet with magic number")
 parser.add_argument("-d", "--debug", help="prints debugging information", action="store_true")
 parser.add_argument("-p", "--port", help="port number of the node", default="6000")
+parser.add_argument("-sg", "--genesis", help="path to shelley genesis file")
 
 args = parser.parse_args()
 myname = os.path.basename(sys.argv[0])
@@ -81,8 +91,10 @@ if args.debug:
 else:
     print_debug = False
     
-
 nodePort = args.port
+parameter = get_genesis(args.genesis)
+
+networkId = parameter["networkId"]
 
 #if args.config:
 #    my_config = getconfig(args.config)
@@ -108,7 +120,18 @@ nodeVersion = nodeInfoLine1[1]
 nodeRevision = nodeInfoLine2[2][:8]
 
 # get metrics in JSON via EKG
-response = requestmetric(METRICS_URL)
+metrics = requestmetric(METRICS_URL)
+#print(response.encoding)
+
+epoch = metrics["cardano"]["node"]["metrics"]["epoch"]["int"]["val"]
+blockNum = metrics["cardano"]["node"]["metrics"]["blockNum"]["int"]["val"]
+slotInEpoch = metrics["cardano"]["node"]["metrics"]["slotInEpoch"]["int"]["val"]
+slotNum = metrics["cardano"]["node"]["metrics"]["slotNum"]["int"]["val"]
+density = metrics["cardano"]["node"]["metrics"]["density"]["real"]["val"]
+forks = metrics["cardano"]["node"]["metrics"]["forks"]["int"]["val"]
+txsProcessedNum = metrics["cardano"]["node"]["metrics"]["txsProcessedNum"]["int"]["val"]
+txsInMempool = metrics["cardano"]["node"]["metrics"]["txsInMempool"]["int"]["val"]
+mempoolBytes = metrics["cardano"]["node"]["metrics"]["mempoolBytes"]["int"]["val"]
 
 #response_list = response.text.splitlines(True)
 #metrics = {}
@@ -124,5 +147,8 @@ if aboutToLead == 0:
 else:
         nodeMode = "Core"
 
-print("\t> " + NODE_NAME + " - (" + nodeMode + " - Mainnet) : " + nodeVersion + " [" + nodeRevision + "] <")
+print("\t> " + NODE_NAME + " - (" + nodeMode + " - " + networkId + ") : " + nodeVersion + " [" + nodeRevision + "] <")
 print("Uptime: " + " | Port: " + nodePort + " | ADAAT " + myname + " " + VERSION + " |")
+print("Epoch " + str(epoch) + " [" + "], " + " remaining")
+print("Block\t: " + str(blockNum) + "\tTip (ref)\t:" + "\tForks\t\t: " + str(forks))
+print("Slot\t: " + str(slotNum) + "\tTip (diff)\t:" + "\tTotal Tx\t: " + str(txsProcessedNum))
